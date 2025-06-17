eb56f0765223fca74aba8aee7a599a9e2d15349c
DamGarcia <damg883@gmail.com>
2025-06-17 12:10:09 -0400

    worked on project in-class added models and controllers

--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
@@ -1,4 +1,46 @@
 package com.pluralsight.NorthwindTradersAPI.controllers;
 
+import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class ProductsController {
+    
+    @RequestMapping(path="/products", method= RequestMethod.GET)
+    public List<Product> getAllProducts(){
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Milk", 1, 5.99));
+        products.add(new Product(2, "Apples", 1, 5.99));
+        products.add(new Product(3, "Water", 1, 5.99));
+        products.add(new Product(4, "Shoes", 2, 5.99));
+        products.add(new Product(5, "Shirt", 2, 5.99));
+        
+        return products;
+    }
+
+
+    @RequestMapping(path="/products/{id}", method= RequestMethod.GET)
+    public List<Product> getProducts(@PathVariable int id){
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Milk", 1, 5.99));
+        products.add(new Product(2, "Apples", 1, 5.99));
+        products.add(new Product(3, "Water", 1, 5.99));
+        products.add(new Product(4, "Shoes", 2, 5.99));
+        products.add(new Product(5, "Shirt", 2, 5.99));
+
+        for(Product p : products){
+            if(p.getProductID() == id){
+                ArrayList<Product> resultProducts = new ArrayList<>();
+                resultProducts.add(p);
+                return resultProducts;
+            }
+        }
+        
+        return new ArrayList<Product>();
+    }
+    
+    
 }
--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/models/Product.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/models/Product.java
@@ -1,4 +1,63 @@
 package com.pluralsight.NorthwindTradersAPI.models;
 
-public class Product {
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private int productID;
+    private String productName;
+    private int categoryID;
+    private double UnitPrice;
+
+    public Product(int productID, String productName, int categoryID, double unitPrice) {
+        this.productID = productID;
+        this.productName = productName;
+        this.categoryID = categoryID;
+        UnitPrice = unitPrice;
+    }
+
+    public Product() { 
+        // do nothing
+    }
+    
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public double getUnitPrice() {
+        return UnitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        UnitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productID=" + productID +
+                ", productName='" + productName + '\'' +
+                ", categoryID=" + categoryID +
+                ", UnitPrice=" + UnitPrice +
+                '}';
+    }
 }
